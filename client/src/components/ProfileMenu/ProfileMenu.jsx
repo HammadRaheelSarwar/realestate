9aeba8dab4ad8e819d59dd744a5802644302e0cc
Hammad Raheel Sarwar <hammadraheel.cs@gmail.com>
2026-07-27 20:02:55 +0600

    .

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -9,11 +9,11 @@ const ProfileMenu = ({user, logout}) => {
             <Avatar src={user?.picture} alt='user image' radius={"xl"}/>
         </Menu.Target>
         <Menu.Dropdown>
-            <Menu.Item onClick={()=> navigate("./favourites", {replace: true})}>
+            <Menu.Item onClick={()=> navigate("/favourites", {replace: true})}>
                 Favourites
             </Menu.Item>
 
-            <Menu.Item onClick={()=> navigate("./bookings", {replace: true})}>
+            <Menu.Item onClick={()=> navigate("/bookings", {replace: true})}>
                 Bookings
             </Menu.Item>
 
